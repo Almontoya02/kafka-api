f50e5850cc19cc1df5884739c1cf5bb1270542a0
Alejandro Montoya <almontoya02@outlook.com>
2022-06-01 16:08:37 -0500

    deploy heroku

--- kafka.js
+++ kafka.js
@@ -1,7 +1,7 @@
 const { Kafka } = require('kafkajs')
 
-username="6ARGYT7HFHQQ5XSO"
-password="8hO3ZVpbvBmsUHoc1tYqHPLUyjghXCSdLvy4UfYTkmOux6+ur5iAspJFhD2C5TMB"
+username="6ZVSGUCQ2ORS25EA"
+password="WlF+jQzozFCcBozuWYCLF8RFOEWUDb0N729qvXSZmYVRN2ipoTIvGQQEmB8wHa4V"
 const sasl = username && password ? { username, password, mechanism: 'plain' } : null
 const ssl = !!sasl
 
@@ -9,7 +9,7 @@ const ssl = !!sasl
 // the environment variable KAFKA_BOOTSTRAP_SERVER
 const kafka = new Kafka({
   clientId: 'npm-slack-notifier',
-  brokers: ["pkc-ldjyd.southamerica-east1.gcp.confluent.cloud:9092"],
+  brokers: ["pkc-6ojv2.us-west4.gcp.confluent.cloud:9092"],
   ssl,
   sasl
 })
