8b31c2d200cbcbe63fc7e443960f31292802e8c7
Alejandro Montoya <almontoya02@outlook.com>
2022-06-01 18:56:54 -0500

    update

--- kafka.js
+++ kafka.js
@@ -4,7 +4,7 @@ username="U3WLHGITZ7QODJ52"
 password="eabTVDGCyvpppxdwO+eYzhfVZURUZ/phWnWCawaiIPUL24P85wn03T3qIsBM3BjY"
 const sasl = username && password ? { username, password, mechanism: 'plain' } : null
 const ssl = !!sasl
-
+//f
 // This creates a client instance that is configured to connect to the Kafka broker provided by
 // the environment variable KAFKA_BOOTSTRAP_SERVER
 const kafka = new Kafka({
